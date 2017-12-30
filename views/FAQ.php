8a1ac3548afba04986243c87378ee4e426fb6822
Anthony <darknoon@darkcity.fr>
2017-12-30 21:44:16 +0100

    Remplacement des images dans la FAQ

--- views/FAQ.php
+++ views/FAQ.php
@@ -27,8 +27,8 @@ require_once("views/page_header.php");
 			<br>
 
 			<?php if ($Utype == "internal") { ?>
-				<strong>Première chose à faire: s'inscrire sur la cartographie: <font color="#FF0000"><?php echo $servername; ?></font><br><br>
-				<img alt="http://img364.imageshack.us/img364/9620/faq00mx8.jpg" src="http://img364.imageshack.us/img364/9620/faq00mx8.jpg" width="402" height="279"><br>
+				<strong>Première chose à faire: s'inscrire sur la cartographie: <span style="color: #FF0000; "><?php echo $servername; ?></span><br><br>
+				<img src="https://wiki.ogsteam.fr/lib/exe/fetch.php?media=fr:market:inscription.png" width="402" height="279"><br>
 				Rien de très compliqué<br><br>
 			<?php } else { ?>
 				<strong>Première chose à faire: s'inscrire sur le forum de <?php echo $NForum; ?>:
@@ -43,18 +43,18 @@ require_once("views/page_header.php");
 			<?php
 				}
 			?>
-			<strong>Une fois inscrit, logez vous...</strong><br>
+			<strong>Une fois inscrit, connectez vous...</strong><br>
 			<br>
-			<img class="postimg" src="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket06.JPG" alt="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket06.JPG" width="154" height="166"><br>
+			<img class="postimg" src="https://wiki.ogsteam.fr/lib/exe/fetch.php?media=fr:market:connexion.png"  width="100" height="320"><br>
 			<br>
 			<strong>...et choisissez votre univers dans le menu déroulant en haut à gauche
 			de l'écran.</strong><br>
 			<br>
-			<img class="postimg" src="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket07.JPG" alt="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket07.JPG" width="150" height="150"><br>
+			<img class="postimg" src="https://wiki.ogsteam.fr/lib/exe/fetch.php?media=fr:market:univers.png"  width="150" height="150"><br>
 			<br>
 			<strong>Cliquez alors sur &quot;<span style="color: orangered;">Nouvelle offre</span>&quot;.</strong><br>
 			<br>
-			<img class="postimg" src="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket08.JPG" alt="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket08.JPG" width="118" height="178"><br>
+			<img class="postimg" src="https://wiki.ogsteam.fr/lib/exe/fetch.php?media=fr:market:new_offer.png"  width="118" height="178"><br>
 			<br>
 			<br>
 			<strong>Un tableau apparaît alors:</strong><br>
@@ -73,26 +73,19 @@ require_once("views/page_header.php");
 			<strong>Cliquez enfin sur &quot;<span style="color: orangered;">Envoyer</span>&quot; pour
 			mettre votre offre en ligne.</strong><br>
 			<br>
-			<img class="postimg" src="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket09.JPG" alt="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket09.JPG" width="478" height="508"><br>
+			<img class="postimg" src="https://wiki.ogsteam.fr/lib/exe/fetch.php?media=fr:market:offer.png"  width="878" height="308"><br>
 			<br>
 			<strong>Il est bien sur important de remplir de son &quot;<span style="color: orangered;">Profil</span>&quot;
 			(dans le menu de gauche) afin que l'acheteur puisse vous contacter.<br>
 			<br>
 			Il est à noter que l'acheteur voit le taux de change dans la fenêtre principale,
-			donc pas d'entourloupe possible...</strong><br>
-			<br>
-			<img class="postimg" src="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket10.JPG" alt="http://membres.lycos.fr/tibbo30//TutoOGSMarket/OGSMarket10.JPG" width="277" height="43"><br>
-			<br>
-			<strong>Vous pouvez utiliser ce logiciel afin de vous aider dans vos
-			conversions: <a href="http://juper.free.fr/fear/ORC.exe">ORC</a></strong> <em>
-			(peut être faudrait il l'intégrer...)</em><br>
+			donc pas d'entourloupe possible... (ça sent l'arnaque ici : vous ne trouvez pas ? :-) )</strong><br>
 			<br>
 			<strong>Il ne vous reste maintenant plus qu'à patienter.</strong><br>
 			<br>
 			<br>
 			<hr>Note de l'auteur (itea)
 			<hr>
-			<em>(tuto en cours de création, et en attente de la version définitive)</em><br>
 			<br>
 			Voilà, je me suis permis de faire un petit tuto pour expliquer rapidement le
 			principe.<br>
