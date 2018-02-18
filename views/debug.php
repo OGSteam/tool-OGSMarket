66e90a7672c23b387624ca1547af6b64ebe75963
Anthony <darknoon@darkcity.fr>
2018-02-18 16:00:01 +0100

    Ajout Cookies Mode Debug #26

--- views/debug.php
+++ views/debug.php
@@ -15,8 +15,9 @@ if (!defined('IN_OGSMARKET')) {
 // Liste des Variables de session
 echo '<TABLE BORDER=0 CELLPADDING=3 CELLSPACING=1 WIDTH=600 BGCOLOR="#000000" ALIGN="CENTER">';
 echo '<tr bgcolor="#9999CC"><td class="c" colspan="2">Variables de session</td></tr>';
-if (isset($HTTP_SESSION_VARS))
-  foreach ($HTTP_SESSION_VARS as $key=>$value)
+
+if (isset($_SESSION))
+  foreach ($_SESSION as $key=>$value)
   {
     if (is_array($value))
     {
@@ -31,16 +32,16 @@ if (isset($HTTP_SESSION_VARS))
 
 // Liste des variables passées dans l'URL. NB : Il n'y a pas de gestion des tableaux dans ce cas
 echo '<tr  bgcolor="#9999CC"><td class="c" colspan="2">Variables passées en URL</td></tr>';
-if (isset($HTTP_GET_VARS))
-  foreach ($HTTP_GET_VARS as $key=>$value)
+if (isset($_GET))
+  foreach ($_GET as $key=>$value)
   {
     echo "<tr><th nowrap>$key</th><th>$value</td></th>";
   }
 
 // Liste des variables transmises par formulaire
 echo '<tr  bgcolor="#9999CC"><td class="c" colspan="2">Variables passées par formulaire</td></tr>';
-if (isset($HTTP_POST_VARS))
-  foreach ($HTTP_POST_VARS as $key=>$value)
+if (isset($_POST))
+  foreach ($_POST as $key=>$value)
   {
     if (is_array($value))
     {
@@ -52,5 +53,20 @@ if (isset($HTTP_POST_VARS))
    else  
      echo "<tr><th nowrap>$key</td><th>$value</th></tr>";
   }
+// Liste des variables transmises par Cookies
+echo '<tr  bgcolor="#9999CC"><td class="c" colspan="2">Variables passées par Cookies</td></tr>';
+if (isset($_COOKIE))
+    foreach ($_COOKIE as $key=>$value)
+    {
+        if (is_array($value))
+        {
+            echo "<tr><th nowrap>$key</th><th>";
+            foreach ($value as $inckey=>$incval)
+                echo "[$inckey]=>$incval<br />";
+            echo '</th></tr>';
+        }
+        else
+            echo "<tr><th nowrap>$key</td><th>$value</th></tr>";
+    }
 echo '</table>';
 ?>
